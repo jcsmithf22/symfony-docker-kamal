ed039ba0ab90bdae0ebd252f56c401f65ac4db8c
Josiah Smith <jcsmithf22@gmail.com>
2025-10-23 18:51:22 -0400

    fixes

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,7 +9,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 final class ProductController extends AbstractController
